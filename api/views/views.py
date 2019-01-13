7197b86fcc57fac706207fc4387fcd5b9e35e0ad
Solomon Kabaliisa <kabaliisa15@gmail.com>
2019-01-13 11:21:41 +0300

    resolves error

--- api/views/views.py
+++ api/views/views.py
@@ -53,3 +53,13 @@ def get_specific_redflags(id):
        
     return jsonify({'status':404 ,'message': 'Red-flag not found'}),404 
 
+@app.route('/api/v1/redflags/<int:id>/location', methods=['PATCH'])
+def edit_specific_location(id):
+   
+    get_new_location = request.get_json()
+    
+    for redflag in redflags:
+        if redflag.to_json()['id']== id:
+            redflag.to_json()['location'] = get_new_location['location']      
+            return jsonify({'status':200,'id':redflag.to_json()['id'],'message':"Updated red-flag record's location"})       
+    return jsonify({'status':404 ,'message': 'Red-flag not found'}) 
