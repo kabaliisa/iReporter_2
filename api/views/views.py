cf5289fdf33a7b4d91e17d9db835c8aeca099ed8
Solomon Kabaliisa <kabaliisa15@gmail.com>
2019-01-13 09:27:09 +0300

    adds edit-specific-red-flag endpoint[#162905660]

--- api/views/views.py
+++ api/views/views.py
@@ -2,33 +2,13 @@ from flask import jsonify,request
 from api import app
 from api.models.redflags import Incident,redflags
 
-@app.route('/api/v1/redflags', methods=['POST'])
-def add_redflag():
-    data = request.get_json()
-    
-
-    latitude = data['latitude']
-    longitude = data['longitude']
-    
-    image = data['image']
+@app.route('/api/v1/redflags/<int:id>/location', methods=['PATCH'])
+def edit_specific_location(id):
    
-
-    location = {
-        "latitude": latitude,
-        "longitude": longitude
-    }
-
-    try:
-        if type(data['createdBy']) is not str:
-            raise ValueError('This  should be a string')
+    get_new_location = request.get_json()
     
-        redflag = Incident(id, 'createdOn', data['createdBy'],data['incidentType'], location, data['comment'],image)
-        redflags.append(redflag)
-
-    except ValueError:
-        
-        return jsonify({'status':400,'message': 'createdBy should be a string'}), 400
-
-    return jsonify({'status':201,'data': [redflag.to_json()],'message':'Created red-flag record'}),201
-
-
+    for redflag in redflags:
+        if redflag.to_json()['id']== id:
+            redflag.to_json()['location'] = get_new_location['location']      
+            return jsonify({'status':200,'id':redflag.to_json()['id'],'message':"Updated red-flag record's location"})       
+    return jsonify({'status':404 ,'message': 'Red-flag not found'}) 
