b304c2473ca8917db35e8fffecf83b9eddb53ead
Solomon Kabaliisa <kabaliisa15@gmail.com>
2019-01-17 09:16:48 +0300

    updates files

--- api/views/views.py
+++ api/views/views.py
@@ -1,18 +1,15 @@
-from flask import jsonify,request
+from flask import jsonify, request
 from api import app
-from api.models.redflags import Incident,redflags
-
+from api.models.redflags import Incident, redflags
 
 @app.route('/api/v1/redflags', methods=['POST'])
 def add_redflag():
     data = request.get_json()
-    
 
     latitude = data['latitude']
     longitude = data['longitude']
-    
+
     image = data['image']
-   
 
     location = {
         "latitude": latitude,
@@ -20,18 +17,26 @@ def add_redflag():
     }
 
     try:
-        if type(data['createdBy']) is not str:
+        if not isinstance(data['createdBy'], str):
             raise ValueError('This  should be a string')
-    
-        redflag = Incident(id, 'createdOn', data['createdBy'],data['incidentType'], location, data['comment'],image)
+
+        redflag = Incident(
+            id,
+            'createdOn',
+            data['createdBy'],
+            data['incidentType'],
+            location,
+            data['comment'],
+            image)
         redflags.append(redflag)
 
     except ValueError:
-        
-        return jsonify({'status':400,'message': 'createdBy should be a string'}), 400
 
-    return jsonify({'status':201,'data': [redflag.to_json()],'message':'Created red-flag record'}),201
+        return jsonify(
+            {'status': 400, 'message': 'createdBy should be a string'}), 400
 
+    return jsonify({'status': 201, 'data': [
+                   redflag.to_json()], 'message': 'Created red-flag record'}), 201
 
 
 @app.route('/api/v1/redflags', methods=['GET'])
@@ -41,33 +46,37 @@ def get_all_redflags():
         reds.append(redflag.to_json())
 
     if len(reds) < 1:
-        return jsonify({'status':404,'message': 'There are no red-flags'})
-    return jsonify({'status':200,'data': reds}),200
+        return jsonify({'status': 404, 'message': 'There are no red-flags'})
+    return jsonify({'status': 200, 'data': reds}), 200
 
 
 @app.route('/api/v1/redflags/<int:id>', methods=['GET'])
 def get_specific_redflags(id):
-    for redflag in redflags:  
-        if redflag.to_json()['id']== id:
-            return jsonify({'status':200,'data':redflag.to_json()}),200
-       
-    return jsonify({'status':404 ,'message': 'Red-flag not found'}),404 
+    for redflag in redflags:
+        if redflag.to_json()['id'] == id:
+            return jsonify({'status': 200, 'data': redflag.to_json()}), 200
+
+    return jsonify({'status': 404, 'message': 'Red-flag not found'}), 404
+
 
 @app.route('/api/v1/redflags/<int:id>/location', methods=['PUT'])
 def edit_specific_location(id):
     get_new_location = request.get_json()
-    
+
     for redflag in redflags:
-        if redflag.to_json()['id']== id:
-            redflag.to_json()['location'] = get_new_location['location']      
-            return jsonify({'status':200,'id':redflag.to_json()['id'],'message':"Updated red-flag record's location"})       
-    return jsonify({'status':404 ,'message': 'Red-flag not found'}),404 
+        if redflag.to_json()['id'] == id:
+            redflag.to_json()['location'] = get_new_location['location']
+            return jsonify({'status': 200, 'id': redflag.to_json()[
+                           'id'], 'message': "Updated red-flag record's location"})
+    return jsonify({'status': 404, 'message': 'Red-flag not found'}), 404
+
 
 @app.route('/api/v1/redflags/<int:id>', methods=['DELETE'])
 def remove_specific_redflag(id):
     for redflag in redflags:
-        if redflag.to_json()['id']== id:
+        if redflag.to_json()['id'] == id:
             redflags.remove(redflag)
         else:
-            return jsonify({'status':404,'message':'not found'}),404    
-    return jsonify({'status':200 ,'id':redflag.to_json()['id'],'message': 'red-flag record has been deleted'}),200
+            return jsonify({'status': 404, 'message': 'not found'}), 404
+    return jsonify({'status': 200, 'id': redflag.to_json()[
+                   'id'], 'message': 'red-flag record has been deleted'}), 200
--- tests/test_redflags.py
+++ tests/test_redflags.py
@@ -5,7 +5,6 @@ from api.views import views
 from api.models.redflags import Incident
 
 
-
 class TestRedflag(unittest.TestCase):
 
     def setUp(self):
@@ -14,33 +13,48 @@ class TestRedflag(unittest.TestCase):
     def test_add_redflag(self):
         """Tests add_redflag method"""
 
-        input_data = {  'createdBy': 'solo','incidentType':'nepotism', 'latitude':'6336377','longitude':'676778','image':["fyf","hhshs"],'comment':'gdihdiududh','image':['duck.jpg','ffggg']}
+        input_data = {
+            'createdBy': 'solo',
+            'incidentType': 'nepotism',
+            'latitude': '6336377',
+            'longitude': '676778',
+            'image': [
+                "fyf",
+                "hhshs"],
+            'comment': 'gdihdiududh',
+            'image': [
+                'duck.jpg',
+                'ffggg']}
         response = self.app_tester.post('/api/v1/redflags', json=input_data)
         self.assertEqual(response.status_code, 201)
 
     def test_model_function(self):
         """Tests if dummy data provided is instance of class Incident"""
 
-        self.redflag = Incident(id=1,createdOn="1/jan/2018",createdBy="solo",incidentType="bribe",location="kla",comment="mmnnucuud",image="sd.jpg")
-        self.assertIsInstance(self.redflag,Incident)
+        self.redflag = Incident(
+            id=1,
+            createdOn="1/jan/2018",
+            createdBy="solo",
+            incidentType="bribe",
+            location="kla",
+            comment="mmnnucuud",
+            image="sd.jpg")
+        self.assertIsInstance(self.redflag, Incident)
 
     def test_get_all_redflags(self):
         """Test get_all_redflags method"""
 
         response = self.app_tester.get("/api/v1/redflags")
-        self.assertEqual(response.status_code,200)
- 
+        self.assertEqual(response.status_code, 200)
+
     def test_get_specific_redflags(self):
         """Test get_specific_redflags method"""
 
         response = self.app_tester.get("/api/v1/redflags/1")
-        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.status_code, 200)
 
-    
     def test_remove_specific_redflag(self):
         """Test remove_specific_redflag method"""
-     
+
         response = self.app_tester.delete("/api/v1/redflags/1")
-        self.assertEqual(response.status_code,200)
-
-
+        self.assertEqual(response.status_code, 200)
